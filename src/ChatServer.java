35b2958e9b0ccaaf8c43a1968719d5ec302cfebc
Adrien <adrien.thiery@gmail.com>
2012-11-12 16:16:14 +0100

    Implémentation des getters, setters et fonctions add()

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,14 +1,45 @@
-import java.net.InetAddress;
-import java.rmi.*;
 import java.util.*;
-import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ChatServer extends UnicastRemoteObject implements Chat {
-	LinkedList<Client> listeClients;
-	LinkedList<String> historiqueMessages;
+	protected LinkedList<Client> listeClients;
+	protected LinkedList<String> historiqueMessages;
+	protected int port;
 	
-	// Implémentation du constructeur
+	/* MÃ©thodes liÃ©es Ã  l'argument listeClients */
+	public LinkedList<Client> getListeClients() {
+		return listeClients;
+	}
+	public void setListeClients(LinkedList<Client> listeClients) {
+		this.listeClients = listeClients;
+	}
+	public void addClient(Client client) {
+		this.listeClients.addLast(client);
+	}
+	public void removeClient(Client client) {
+		this.listeClients.remove(client);
+	}
+
+	/* MÃ©thodes liÃ©es Ã  l'historique des messages */
+	public LinkedList<String> getHistoriqueMessages() {
+		return historiqueMessages;
+	}
+	public void setHistoriqueMessages(LinkedList<String> historiqueMessages) {
+		this.historiqueMessages = historiqueMessages;
+	}
+	public void addMessage(String message, Client auteur) {
+		this.historiqueMessages.addLast(auteur+" : "+message);
+	}
+	
+	/* MÃ©thodes du port */
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	// ImplÃ©mentation du constructeur
 	public ChatServer() throws java.rmi.RemoteException {
 		LinkedList<Client> liste = new LinkedList<Client>();
 		LinkedList<String> historique = new LinkedList<String>();
@@ -16,7 +47,7 @@ public class ChatServer extends UnicastRemoteObject implements Chat {
 		historiqueMessages = historique;
 	}
 	
-	// Implémentation de la méthode distante
+	// ImplÃ©mentation de la mÃ©thode distante
 	public void afficherHistoriqueMessage() throws java.rmi.RemoteException {
 		for(String s:historiqueMessages)
 			System.out.println(s);
@@ -39,21 +70,5 @@ public class ChatServer extends UnicastRemoteObject implements Chat {
 		return listeClients;
 	}
 	
-	public static void main(String args[]) {
-		int port; String URL;
-		try { // transformation d ’une chaîne de caractères en entier
-			Integer I = new Integer(args[0]); port = I.intValue();
-		} catch (Exception ex) {
-			System.out.println(" Please enter: Server <port>"); return;
-		}
-		try {
-			// Création du serveur de nom - rmiregistry
-			Registry registry = LocateRegistry.createRegistry(port);
-			// Création d ’une instance de l’objet serveur
-			Chat obj = new ChatServer();
-			// Calcul de l’URL du serveur
-			URL = "//"+InetAddress.getLocalHost().getHostName()+":"+port+"/ChatServer";
-			Naming.rebind(URL, obj);
-		} catch (Exception exc) { }
-	}
+	
 }
