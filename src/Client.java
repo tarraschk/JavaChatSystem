1a538781554da286167f18704967537a263b95c6
Adrien <adrien.thiery@gmail.com>
2012-11-12 17:01:46 +0100

    Codage du main Client

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,14 @@ import java.util.*;
 
 public class Client {
 	protected int id;
+	protected boolean connectStatus;
 	
+	public boolean getConnectStatus() {
+		return connectStatus;
+	}
+	public void setConnectStatus(boolean connectStatus) {
+		this.connectStatus = connectStatus;
+	}
 	/* Getters et setters de id */
 	public int getId() {
 		return id;
@@ -18,19 +25,47 @@ public class Client {
 	 * @throws IOException
 	 * @throws java.rmi.RemoteException
 	 */
-	public void lire() throws IOException, java.rmi.RemoteException{
+	public String[] lire() throws IOException, java.rmi.RemoteException{
+		System.out.println("Entrer une commande :");
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 		String cmd = keyboard.readLine();
 		if (cmd.indexOf(' ') != -1) {
-			String command = cmd.substring(0, cmd.indexOf(' '));
-			String message = cmd.substring(cmd.indexOf(' '));
-			System.out.println("Commande : "+command+" - "+message);
+			String[] commande = new String[2];
+			commande[0] = cmd.substring(0, cmd.indexOf(' '));
+			commande[1] = cmd.substring(cmd.indexOf(' '));
+			System.out.println("Commande : "+commande[0]+" - "+commande[1]);
+			
+			return commande;
+		}
+		else {
+			System.out.println("Erreur de commande");
+			return null;
 		}
 	}
 	
 	public static void main(String args[]) throws IOException {
 		Client Clienttest = new Client();
-		Clienttest.lire();
+		String[] commandeMessage = new String[2];
+		commandeMessage = Clienttest.lire();
+		
+		while(commandeMessage[0] != "bye") {
+			if(commandeMessage[0]=="connect"){
+				Clienttest.setConnectStatus(connect(commandeMessage[1]));
+			}
+			else if(commandeMessage[0]=="send" && Clienttest.getConnectStatus()){
+				send(commandeMessage[1]);
+			}
+			else if(commandeMessage[0]=="who" && Clienttest.getConnectStatus()) {
+				who();
+			}
+			else if((commandeMessage[0]=="who" || commandeMessage[0]=="send") && !Clienttest.getConnectStatus()) {
+				System.out.println("Veuillez vous connecter auparavant");
+			}
+			
+			commandeMessage = Clienttest.lire();
+		}
+		
+		System.out.println("A bientôt !");
 		
 	}
 }
