7e68d06998d8b8dcd0d6754e70201508aa2986cd
Adrien <adrien.thiery@gmail.com>
2012-11-12 15:53:30 +0100

    Implémentation du main pour créer le server

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,5 +1,7 @@
+import java.net.InetAddress;
 import java.rmi.*;
 import java.util.*;
+import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ChatServer extends UnicastRemoteObject implements Chat {
@@ -7,7 +9,7 @@ public class ChatServer extends UnicastRemoteObject implements Chat {
 	LinkedList<String> historiqueMessages;
 	
 	// Implémentation du constructeur
-	public ChatServer(String msg) throws java.rmi.RemoteException {
+	public ChatServer() throws java.rmi.RemoteException {
 		LinkedList<Client> liste = new LinkedList<Client>();
 		LinkedList<String> historique = new LinkedList<String>();
 		listeClients = liste;
@@ -35,5 +37,23 @@ public class ChatServer extends UnicastRemoteObject implements Chat {
 	public LinkedList<Client> who() {
 		
 		return listeClients;
+	}
+	
+	public static void main(String args[]) {
+		int port; String URL;
+		try { // transformation d ’une chaîne de caractères en entier
+			Integer I = new Integer(args[0]); port = I.intValue();
+		} catch (Exception ex) {
+			System.out.println(" Please enter: Server <port>"); return;
+		}
+		try {
+			// Création du serveur de nom - rmiregistry
+			Registry registry = LocateRegistry.createRegistry(port);
+			// Création d ’une instance de l’objet serveur
+			Chat obj = new ChatServer();
+			// Calcul de l’URL du serveur
+			URL = "//"+InetAddress.getLocalHost().getHostName()+":"+port+"/ChatServer";
+			Naming.rebind(URL, obj);
+		} catch (Exception exc) { }
 	}
 }
