a3156b51ff9676951149423c58b5bab94d174efb
Adrien <adrien.thiery@gmail.com>
2012-11-16 10:10:38 +0100

    Ajout de l'affichage dans client

--- src/Client.java
+++ src/Client.java
@@ -95,9 +95,7 @@ public class Client  implements Serializable {
 			}
 			else if(commandeMessage[0].equals("send") && clienttest.getConnectStatus()){
 				clienttest.getServChat().send(commandeMessage[1],clienttest);
-				for(String s : ((ChatServer) clienttest.getServChat()).getHistoriqueMessages()){
-					System.out.println(s);
-				}
+				clienttest.getServChat().afficher();
 			}
 			else if(commandeMessage[0].equals("who") && clienttest.getConnectStatus()) {
 				clienttest.getServChat().who();
